ffd0afe0747614736e8f4e2ad52a3152ddd1ea84
mark455174@gmail.com <mark123456>
2022-06-10 13:36:25 +0800

    6/10 commit

---
+++ src/main/java/com/Mark/KotlinPerson.kt
@@ -0,0 +1,13 @@
+package com.Mark
+
+class KotlinPerson {
+    fun greeting(){
+        println("Hello Kotlin")
+    }
+}
+
+//1st level
+fun main(){
+    val p = KotlinPerson()
+    p.greeting()
+}
---
+++ src/main/java/com/Mark/Person.java
@@ -0,0 +1,7 @@
+package com.Mark;
+
+public class Person {
+    public void greeting(){
+        System.out.println("Good Day!");
+    }
+}
